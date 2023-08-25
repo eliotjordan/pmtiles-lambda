609ea9779ba5e6803ff6939497143934b30932ec
Eliot Jordan <eliotj@princeton.edu>
2023-08-25 13:37:59 -0500

    Basic lambda

--- app.py
+++ app.py
@@ -5,24 +5,20 @@ import aws_cdk as cdk
 
 from pmtiles_lambda.pmtiles_lambda_stack import PmtilesLambdaStack
 
-
 app = cdk.App()
-PmtilesLambdaStack(app, "PmtilesLambdaStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+PmtilesLambdaStack(
+    app,
+    "pmtiles-staging",
+    stage="staging",
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+)
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+PmtilesLambdaStack(
+    app,
+    "pmtiles-production",
+    stage="production",
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+)
 
 app.synth()
--- pmtiles_lambda/pmtiles_lambda_stack.py
+++ pmtiles_lambda/pmtiles_lambda_stack.py
@@ -1,19 +1,53 @@
+import os
+import aws_cdk as cdk
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_s3 as s3,
+    aws_apigatewayv2,
+    aws_lambda,
+    aws_events,
+    aws_events_targets
 )
 from constructs import Construct
 
 class PmtilesLambdaStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, stage: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        lambda_function = aws_lambda.DockerImageFunction(
+            self,
+            f"pmtiles-{stage}-PmtilesFuntion",
+            code=aws_lambda.DockerImageCode.from_image_asset(
+                os.path.abspath("./resources/"),
+                file="Dockerfile",
+                build_args={"platform" : "linux/amd64"},
+                cmd=[ "handler.handler" ],
+                entrypoint=["/lambda-entrypoint.sh"],
+            ),
+            memory_size=3008,
+            timeout=Duration.seconds(600)
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PmtilesLambdaQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # S3 Permissions
+        permission = iam.PolicyStatement(
+            actions=["s3:GetObject"],
+            resources=[
+                f"arn:aws:s3:::figgy-geo-{stage}/*",
+                "arn:aws::s3:::pul-tile-images/*",
+                "arn:aws:s3:::*/*"
+            ]
+        )
+        lambda_function.add_to_role_policy(permission)
+
+        # Add function url to primary lambda
+        # Use instead of API gateway to bypass 30 second gateway timeout limit
+        lambda_url = lambda_function.add_function_url(
+            auth_type=aws_lambda.FunctionUrlAuthType.NONE
+        )
+        function_url = cdk.Fn.select(2, cdk.Fn.split('/', lambda_url.url))
+
+        cdk.CfnOutput(self, "Function URL", value=function_url)
+
